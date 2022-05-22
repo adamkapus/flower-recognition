b8dd05be7d02a3615b8289846c4ec67a2f65fd08
adamkapus <kapusadam03@gmail.com>
2022-05-23 00:43:43 +0200

    add: native display name for every flower

--- AndroidApplication/FlowerRecognition/app/src/main/java/hu/bme/aut/flowerrecognition/util/FlowerResolver.kt
+++ AndroidApplication/FlowerRecognition/app/src/main/java/hu/bme/aut/flowerrecognition/util/FlowerResolver.kt
@@ -7,29 +7,58 @@ class FlowerResolver {
     private var flowerRarityMap = HashMap<String, Rarity>()
 
     init {
+        flowerDisplayNameMap["bluebell"] = R.string.flower_name_bluebell
+        flowerRarityMap["bluebell"] = Rarity.COMMON
+
+        flowerDisplayNameMap["buttercup"] = R.string.flower_name_buttercup
+        flowerRarityMap["buttercup"] = Rarity.COMMON
+
+        flowerDisplayNameMap["colts foot"] = R.string.flower_name_colts_foot
+        flowerRarityMap["colts foot"] = Rarity.COMMON
+
+        flowerDisplayNameMap["cowslip"] = R.string.flower_name_cowslip
+        flowerRarityMap["cowslip"] = Rarity.RARE
+
+        flowerDisplayNameMap["crocus"] = R.string.flower_name_crocus
+        flowerRarityMap["crocus"] = Rarity.SUPER_RARE
+
+        flowerDisplayNameMap["daffodil"] = R.string.flower_name_daffodil
+        flowerRarityMap["daffodil"] = Rarity.COMMON
+
+        flowerDisplayNameMap["daisy"] = R.string.flower_name_daisy
+        flowerRarityMap["daisy"] = Rarity.COMMON
+
+        flowerDisplayNameMap["dandelion"] = R.string.flower_name_dandelion
+        flowerRarityMap["dandelion"] = Rarity.COMMON
+
+        flowerDisplayNameMap["fritillary"] = R.string.flower_name_fritillary
+        flowerRarityMap["fritillary"] = Rarity.SUPER_RARE
+
+        flowerDisplayNameMap["iris"] = R.string.flower_name_iris
+        flowerRarityMap["iris"] = Rarity.RARE
+
+        flowerDisplayNameMap["lily valley"] = R.string.flower_name_lily_valley
+        flowerRarityMap["lily valley"] = Rarity.RARE
+
+        flowerDisplayNameMap["pansy"] = R.string.flower_name_pansy
+        flowerRarityMap["pansy"] = Rarity.COMMON
+
         flowerDisplayNameMap["snowdrop"] = R.string.flower_name_snowdrop
         flowerRarityMap["snowdrop"] = Rarity.SUPER_RARE
 
         flowerDisplayNameMap["sunflower"] = R.string.flower_name_sunflower
         flowerRarityMap["sunflower"] = Rarity.RARE
 
-        flowerDisplayNameMap["dandelion"] = R.string.flower_name_dandelion
-        flowerRarityMap["dandelion"] = Rarity.COMMON
+        flowerDisplayNameMap["tiger lily"] = R.string.flower_name_tiger_lily
+        flowerRarityMap["tiger lily"] = Rarity.SUPER_RARE
+
 
         flowerDisplayNameMap["tulip"] = R.string.flower_name_tulip
         flowerRarityMap["tulip"] = Rarity.RARE
 
-        flowerDisplayNameMap["pansy"] = R.string.flower_name_pansy
-        flowerRarityMap["pansy"] = Rarity.COMMON
+        flowerDisplayNameMap["windflower"] = R.string.flower_name_windflower
+        flowerRarityMap["windflower"] = Rarity.RARE
 
-        flowerDisplayNameMap["iris"] = R.string.flower_name_iris
-        flowerRarityMap["iris"] = Rarity.RARE
-
-        flowerDisplayNameMap["daisy"] = R.string.flower_name_daisy
-        flowerRarityMap["daisy"] = Rarity.COMMON
-
-        flowerDisplayNameMap["crocus"] = R.string.flower_name_crocus
-        flowerRarityMap["crocus"] = Rarity.SUPER_RARE
     }
 
     fun getDisplayName(label: String?): Int {
